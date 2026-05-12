e0589ae2e499750edbd6af7eb830c1adfe886b5c
sachinprasad3 <sachinprasad841@gmail.com>
2026-05-12 11:50:32 +0530

    DLT-changes

--- notification-service/src/main/java/com/notification/KafkaConsumer.java
+++ notification-service/src/main/java/com/notification/KafkaConsumer.java
@@ -1,6 +1,9 @@
 package com.notification;
 
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,20 +19,31 @@ public class KafkaConsumer {
 		System.out.println("message from order : "+msg);
 	}
 	
+	
+	
+	@RetryableTopic(attempts = "3", // Initial try + 2 retries
+			backoff = @Backoff(delay = 2000)
+			)
+	
 	@KafkaListener(topics = "order-event", groupId = "my-group")
 	public void consumeEvent(OrderEvent event) {
 		System.out.println("================================");
 
         System.out.println("Notification Received");
-
         System.out.println("Order ID : " + event.getOrderId());
-
         System.out.println("Product : " + event.getProductName());
-
         System.out.println("Amount : " + event.getAmount());
-
         System.out.println("================================");
+        
+        throw new RuntimeException("Failure");
 		
+		
+	}
+	
+	@DltHandler
+	public void dlt(OrderEvent event) {
+		System.out.println("---DLT----");
+		System.out.println("Product : " + event.getProductName());
 		
 	}
 	
